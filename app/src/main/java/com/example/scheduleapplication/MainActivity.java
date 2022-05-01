080d4205d2244b178c8ba5d9879f1c362d067f72
nahla.baalbaki <nahla.baalbaki@lau.edu>
2022-05-01 18:35:09 +0300

    CreatedDB

--- app/src/main/java/com/example/scheduleapplication/MainActivity.java
+++ app/src/main/java/com/example/scheduleapplication/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.scheduleapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            SQLiteDatabase sql = this.openOrCreateDatabase("scheduleapp",MODE_PRIVATE,null);
+            sql.execSQL("CREATE Table IF NOT EXISTS courses(name VARCHAR, website VARCHAR)");
+
+            sql.execSQL("INSERT INTO courses(name,wesbite) VALUES ('Mobile Development','https://ionicframework.com/')");
+            sql.execSQL("INSERT INTO courses(name,wesbite) VALUES ('Game Programming','https://unity.com/')");
+            sql.execSQL("INSERT INTO courses(name,wesbite) VALUES ('Parallel Programming','https://www.open-mpi.org/')");
+            sql.execSQL("INSERT INTO courses(name,wesbite) VALUES ('Natural Language Processing','https://www.nlp.com/what-is-nlp/')");
+
+
+        }catch(Exception e){
+
+        }
+        
     }
 }
